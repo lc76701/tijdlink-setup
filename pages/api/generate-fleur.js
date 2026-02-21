7dea301b665bcf22b82d3c19a1a3d77cd44605b2
lc76701 <info@easyclickmedia.com>
2026-02-22 00:13:34 +0100

    Update target URL in generate-fleur API

--- pages/api/generate-fleur.js
+++ pages/api/generate-fleur.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   } while (await redis.get(`slug-${slug}`));
 
   await redis.set(`slug-${slug}`, {
-    target: "https://tikkie.me/pay/dkn9ljjpt4ls6fd82cio",
+    target: "https://tikkie.me/verlopen",
     flow: "normal",
     firstClick: null,
   });
