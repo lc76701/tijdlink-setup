67d745d9ffff8bf66800475fa818cee1e2e4a591
lc76701 <info@easyclickmedia.com>
2026-04-08 19:19:13 +0200

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   } while (await redis.get(`slug-${slug}`));
 
   await redis.set(`slug-${slug}`, {
-    target: "https://tikkie.me/pay/ishlkfbh6f5s1b6gcua9",
+    target: "https://tikkie.me/pay/oat03aesv0sjlqv8lnjd",
     firstClick: null,
   });
 
